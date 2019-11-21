06b103467215e054d835be921aab0a604e49c918
Andriy Knysh <aknysh@users.noreply.github.com>
2019-11-20 23:59:21 -0500

    Convert to TF 0.12. Add Codefresh test pipeline (#2)

--- docs/terraform.md
+++ docs/terraform.md
@@ -2,23 +2,23 @@
 
 | Name | Description | Type | Default | Required |
 |------|-------------|:----:|:-----:|:-----:|
-| attributes | Additional attributes (e.g. `1`) | list | `<list>` | no |
+| attributes | Additional attributes (e.g. `1`) | list(string) | `<list>` | no |
 | delimiter | Delimiter to be used between `name`, `namespace`, `stage` and `attributes` | string | `-` | no |
-| enabled | Set to false to prevent the module from creating any resources | string | `true` | no |
-| expiration_days | Number of days after which to expunge the objects | string | `90` | no |
-| force_destroy | A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable. | string | `false` | no |
-| glacier_transition_days | Number of days after which to move the data to the glacier storage tier | string | `60` | no |
-| lifecycle_prefix | Prefix filter. Used to manage object lifecycle events. | string | `` | no |
-| lifecycle_rule_enabled | Enable lifecycle events on this bucket | string | `true` | no |
-| lifecycle_tags | Tags filter. Used to manage object lifecycle events. | map | `<map>` | no |
+| enabled | Set to false to prevent the module from creating any resources | bool | `true` | no |
+| expiration_days | Number of days after which to expunge the objects | number | `90` | no |
+| force_destroy | A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable | bool | `false` | no |
+| glacier_transition_days | Number of days after which to move the data to the glacier storage tier | number | `60` | no |
+| lifecycle_prefix | Prefix filter. Used to manage object lifecycle events | string | `` | no |
+| lifecycle_rule_enabled | Enable lifecycle events on this bucket | bool | `true` | no |
+| lifecycle_tags | Tags filter. Used to manage object lifecycle events | map(string) | `<map>` | no |
 | name | The Name of the application or solution  (e.g. `bastion` or `portal`) | string | - | yes |
-| namespace | Namespace (e.g. `eg` or `cp`) | string | - | yes |
-| noncurrent_version_expiration_days | Specifies when noncurrent object versions expire. | string | `90` | no |
-| noncurrent_version_transition_days | Specifies when noncurrent object versions transitions | string | `30` | no |
-| region | If specified, the AWS region this bucket should reside in. Otherwise, the region used by the callee. | string | `` | no |
-| stage | Stage (e.g. `prod`, `dev`, `staging`) | string | - | yes |
-| standard_transition_days | Number of days to persist in the standard storage tier before moving to the infrequent access tier | string | `30` | no |
-| tags | Additional tags (e.g. map(`BusinessUnit`,`XYZ`) | map | `<map>` | no |
+| namespace | Namespace (e.g. `eg` or `cp`) | string | `` | no |
+| noncurrent_version_expiration_days | Specifies when noncurrent object versions expire | number | `90` | no |
+| noncurrent_version_transition_days | Specifies when noncurrent object versions transitions | number | `30` | no |
+| region | If specified, the AWS region this bucket should reside in. Otherwise, the region used by the callee | string | `` | no |
+| stage | Stage (e.g. `prod`, `dev`, `staging`) | string | `` | no |
+| standard_transition_days | Number of days to persist in the standard storage tier before moving to the infrequent access tier | number | `30` | no |
+| tags | Additional tags (e.g. map(`BusinessUnit`,`XYZ`) | map(string) | `<map>` | no |
 | traffic_type | The type of traffic to capture. Valid values: `ACCEPT`, `REJECT`, `ALL` | string | `ALL` | no |
 | vpc_id | VPC ID the DB instance will be created in | string | - | yes |
 
--- variables.tf
+++ variables.tf
@@ -1,97 +1,111 @@
 variable "enabled" {
+  type        = bool
   description = "Set to false to prevent the module from creating any resources"
-  default     = "true"
+  default     = true
 }
 
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace (e.g. `eg` or `cp`)"
+  default     = ""
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
+  default     = ""
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "The Name of the application or solution  (e.g. `bastion` or `portal`)"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `name`, `namespace`, `stage` and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Additional attributes (e.g. `1`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
 }
 
 variable "vpc_id" {
-  type        = "string"
+  type        = string
   description = "VPC ID the DB instance will be created in"
 }
 
 variable "lifecycle_prefix" {
-  description = "Prefix filter. Used to manage object lifecycle events."
+  type        = string
+  description = "Prefix filter. Used to manage object lifecycle events"
   default     = ""
 }
 
 variable "lifecycle_tags" {
-  description = "Tags filter. Used to manage object lifecycle events."
+  type        = map(string)
+  description = "Tags filter. Used to manage object lifecycle events"
   default     = {}
 }
 
 variable "region" {
-  description = "If specified, the AWS region this bucket should reside in. Otherwise, the region used by the callee."
+  type        = string
+  description = "If specified, the AWS region this bucket should reside in. Otherwise, the region used by the callee"
   default     = ""
 }
 
 variable "force_destroy" {
-  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
-  default     = "false"
+  type        = bool
+  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable"
+  default     = false
 }
 
 variable "lifecycle_rule_enabled" {
+  type        = bool
   description = "Enable lifecycle events on this bucket"
-  default     = "true"
+  default     = true
 }
 
 variable "noncurrent_version_expiration_days" {
-  description = "Specifies when noncurrent object versions expire."
-  default     = "90"
+  type        = number
+  description = "Specifies when noncurrent object versions expire"
+  default     = 90
 }
 
 variable "noncurrent_version_transition_days" {
+  type        = number
   description = "Specifies when noncurrent object versions transitions"
-  default     = "30"
+  default     = 30
 }
 
 variable "standard_transition_days" {
+  type        = number
   description = "Number of days to persist in the standard storage tier before moving to the infrequent access tier"
-  default     = "30"
+  default     = 30
 }
 
 variable "glacier_transition_days" {
+  type        = number
   description = "Number of days after which to move the data to the glacier storage tier"
-  default     = "60"
+  default     = 60
 }
 
 variable "expiration_days" {
+  type        = number
   description = "Number of days after which to expunge the objects"
-  default     = "90"
+  default     = 90
 }
 
 variable "traffic_type" {
+  type        = string
   description = "The type of traffic to capture. Valid values: `ACCEPT`, `REJECT`, `ALL`"
   default     = "ALL"
 }
