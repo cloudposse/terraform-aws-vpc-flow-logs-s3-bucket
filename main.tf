594de590c572388744faea6f13c4b93f69226d7b
andrewmackett <andrew@yoyowallet.com>
2020-01-07 15:28:51 +0000

    Update main.tf
    Use AWS recommended update key policy
    
    Co-Authored-By: Joe Hosteny <jhosteny@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -19,7 +19,18 @@ data "aws_iam_policy_document" "kms" {
     effect = "Allow"
 
     actions = [
-      "kms:*"
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion"
     ]
 
     resources = [
