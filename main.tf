389ea4e0b54c1ff788ecb22184400f08305238b1
Erik D. Jones <erikdj@jacobianengineering.com>
2020-04-08 16:28:05 -0700

    Added kms:Tag* permission to key to allow tagging

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ data "aws_iam_policy_document" "kms" {
       "kms:Disable*",
       "kms:Get*",
       "kms:Delete*",
+      "kms:Tag*",
       "kms:ScheduleKeyDeletion",
       "kms:CancelKeyDeletion"
     ]
