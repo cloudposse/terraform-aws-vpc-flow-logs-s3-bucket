1f6c117d42d2193723f51032f66eaa8c0b350552
Matt Calhoun <matt@mattcalhoun.com>
2021-06-04 11:15:24 -0400

    add missing }

--- variables.tf
+++ variables.tf
@@ -68,6 +68,7 @@ variable "allow_ssl_requests_only" {
   type        = bool
   default     = false
   description = "Set to `true` to require requests to use Secure Socket Layer (HTTPS/SSL). This will explicitly deny access to HTTP requests"
+}
 
 variable "arn_format" {
   type        = string
