10f6adfdb414903ef63047ddd6335831e5ce3a6d
Brandon Metcalf <brandon.metcalf@umbra.space>
2021-07-16 15:35:35 -0500

    Add permission to allow key to be used to access resources encrypted by key

--- main.tf
+++ main.tf
@@ -8,20 +8,7 @@ data "aws_iam_policy_document" "kms" {
     effect = "Allow"
 
     actions = [
-      "kms:Create*",
-      "kms:Describe*",
-      "kms:Enable*",
-      "kms:List*",
-      "kms:Put*",
-      "kms:Update*",
-      "kms:Revoke*",
-      "kms:Disable*",
-      "kms:Get*",
-      "kms:Delete*",
-      "kms:Tag*",
-      "kms:Untag*",
-      "kms:ScheduleKeyDeletion",
-      "kms:CancelKeyDeletion"
+      "kms:*"
     ]
 
     resources = [
