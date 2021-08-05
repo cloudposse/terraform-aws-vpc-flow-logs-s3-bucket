97a2c848865a55894e933588c0a97f5d11d0973d
Brandon Metcalf <brandon.metcalf@umbra.space>
2021-08-04 23:53:14 -0500

    Remove trailing comma.

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ data "aws_iam_policy_document" "kms" {
       "kms:Tag*",
       "kms:Untag*",
       "kms:ScheduleKeyDeletion",
-      "kms:CancelKeyDeletion",
+      "kms:CancelKeyDeletion"
     ]
 
     resources = [
