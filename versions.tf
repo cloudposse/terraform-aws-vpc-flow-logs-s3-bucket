01e3e6ad4eb9d15d0f88486f5c949452dda88e03
Yonatan Koren <me@yonatankoren.com>
2021-09-09 11:16:34 -0400

    Update context.tf to null-label:0.25.0; bump dependencies to latest; drop unused providers; run make github/init.

--- versions.tf
+++ versions.tf
@@ -2,14 +2,6 @@ terraform {
   required_version = ">= 0.13.0"
 
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.3"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = ">= 2.2"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 2.0"
