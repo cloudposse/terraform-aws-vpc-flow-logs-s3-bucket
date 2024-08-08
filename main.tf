0a3643675c355bbfee331c480db6dda37e7fdedd
Maksym Vlasov <MaxymVlasov@users.noreply.github.com>
2024-08-08 17:47:19 +0300

    feat: Add missed tags

--- main.tf
+++ main.tf
@@ -235,4 +235,6 @@ resource "aws_flow_log" "default" {
   log_format           = var.log_format
   traffic_type         = var.traffic_type
   vpc_id               = var.vpc_id
+
+  tags = module.this.tags
 }
